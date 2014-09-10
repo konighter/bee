ba0441769eb4cd3b0d5461cf294f0ac732a2fe8d
caccoon <caccoon@126.com>
2014-09-10 11:27:37 +0800

    rm complied files

--- bee/tests.py
+++ bee/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
 
 # Create your tests here.
+
+class B:
+     pass
+class C(B):
+     pass
+class D(C):
+     pass
+
+for c in [B, C, D]:
+     try:
+         raise c()
+     except D:
+         print "D"
+     except C:
+         print "C"
+     except B:
+         print "B"
+
+
+
+
+for c in[B,C,D]:
+         try:
+                 raise c()
+         except B:
+                 print "B"
+         except C:
+                 print "C"
+         except D:
+                 print "D"
